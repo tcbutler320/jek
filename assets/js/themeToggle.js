f8f163832a4584e9733bd4ba79859c731db6d2a4
tcbutler320 <tcbutler320@gmail.com>
2021-06-15 20:10:15 -0400

    move all themes to session and local storage

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -21,13 +21,13 @@ function darkMode() {
         document.body.classList.toggle('dark');
         document.getElementById('theme').innerHTML = 'dark';
         console.log('[DEBUG] added dark to session storage');
-        sessionStorage.removeItem('key');
+        // sessionStorage.removeItem('key');
         sessionStorage.setItem('theme', 'dark');
     } else {
         document.body.classList.toggle('light');
         document.getElementById('theme').innerHTML = 'light';
         console.log('[DEBUG] added light to session storage');
-        sessionStorage.removeItem('key');
+        // sessionStorage.removeItem('key');
         sessionStorage.setItem('theme', 'light');
     }
     
@@ -45,9 +45,9 @@ function checkFav() {
     const savedTheme = localStorage.getItem('favTheme');
 
     if (savedTheme){
-    document.body.classList.toggle(savedTheme);    
-    console.log('[DEBUG] theme changed To',savedTheme);
-    document.getElementById('theme').innerHTML = savedTheme;
+        document.body.classList.toggle(savedTheme);    
+        console.log('[DEBUG] theme changed To',savedTheme);
+        document.getElementById('theme').innerHTML = savedTheme;
     } else {
         console.log('[DEBUG] no theme saved');
     }
@@ -68,3 +68,77 @@ function reload(){
     location.reload();
 }
 
+
+/* set custom themes using session storage */
+
+function setCustomBackground() {
+    console.log('[DEBUG] custom backround entered');
+
+    var background = document.getElementById("customBackground").value;
+    let locBackground = localStorage.getItem('customBackground');
+
+    if (locBackground) {
+        localStorage.removeItem('customBackground');
+        localStorage.setItem('customBackground',background);
+        document.getElementById('whatHappened').innerHTML = 'removed previous saved background, set new background to ' + background;
+    } else {
+        localStorage.setItem('customBackground',background);
+        document.getElementById('whatHappened').innerHTML = 'set new background to ' + background;
+    }
+}
+
+function setCustomColor() {
+    console.log('[DEBUG] custom color entered');
+
+    var color = document.getElementById("customColor").value;
+    let locColor = localStorage.getItem('customColor');
+
+    if (locColor) {
+        localStorage.removeItem('customColor');
+        localStorage.setItem('customColor',color);
+        document.getElementById('whatHappened').innerHTML = 'removed previous saved color, set new color to' + color;
+    } else {
+        localStorage.setItem('customColor',color);
+        document.getElementById('whatHappened').innerHTML = 'set new color to ' + color;
+    }
+}
+
+function setCustomTheme() {
+    console.log('[DEBUG] custom theme entered');
+
+    var theme = document.getElementById("customTheme").value;
+    let locTheme = localStorage.getItem('customTheme');
+
+    if (locTheme) {
+        localStorage.removeItem('customTheme');
+        localStorage.setItem('customTheme',theme);
+        document.getElementById('whatHappened').innerHTML = 'removed previous saved theme, set new theme to ' + theme;
+    } else {
+        localStorage.setItem('customTheme',theme);
+        document.getElementById('whatHappened').innerHTML = 'set new theme to ' + theme;
+    }
+
+    setTheme();
+}
+
+function setTheme(){
+    console.log('[DEBUG] now in setTheme');
+
+    let locTheme = localStorage.getItem('theme');
+    let customTheme = localStorage.getItem('customTheme');
+    let sessTheme = sessionStorage.getItem('theme');
+
+    if (customTheme) {
+        document.body.classList.toggle(customTheme);
+        document.getElementById('whatHappened').innerHTML = 'custom theme set: ' + customTheme;
+    } else if (locTheme) {
+        document.body.classList.toggle(locTheme);
+        document.getElementById('whatHappened').innerHTML = 'favorite theme found: ' + locTheme;
+    } else if (sessTheme) {
+        document.body.classList.toggle(sessTheme);
+        document.getElementById('whatHappened').innerHTML = 'session theme found: ' + sessTheme;
+    } else {
+        document.body.classList.toggle('light');
+        document.getElementById('whatHappened').innerHTML = 'no theme saved, default to light';
+    }
+}
